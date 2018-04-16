e5a88946d7e23b30b763aab3d564805ef69ef8fa
Sara_ADR <sara_adr@hotmail.com>
2018-04-16 02:28:57 +0100

    commit4

--- AFrameProject/app.js
+++ AFrameProject/app.js
@@ -1,9 +1,9 @@
-/*Components modularize code, make logic and behavior visible from HTML, 
+/*Components modularize code, make logic and behavior visible from HTML,
  and ensure that code is executed at the correct time*/
 
 AFRAME.registerComponent('log', {
    schema: {type: 'string'},
-   
+
    init: function(){
         var stringToLog = this.data;
         console.log(stringToLog)
@@ -11,7 +11,7 @@ AFRAME.registerComponent('log', {
 });
 
 //Volume component
-AFRAME.registerComponent('volume', {		
+AFRAME.registerComponent('volume', {
     schema: {
         volumePath: {type: 'string'}
     },
@@ -20,34 +20,34 @@ AFRAME.registerComponent('volume', {
 		this.volume = volume;
 		console.log(volume);
 
-		var extractedx = volume.extractPerpendicularPlane( 'x', (volume.RASDimensions[0]/4) );	
-		var extractedy = volume.extractPerpendicularPlane( 'y', (volume.RASDimensions[1]/2) );	
+        var entityEx = document.createElement('a-entity');
+        var entityEy = document.createElement('a-entity');
+        var entityEz = document.createElement('a-entity');
+
+		var extractedx = volume.extractPerpendicularPlane( 'x', (volume.RASDimensions[0]/4) );
+		var extractedy = volume.extractPerpendicularPlane( 'y', (volume.RASDimensions[1]/2) );
 		var extractedz = volume.extractPerpendicularPlane( 'z', (volume.RASDimensions[2]/2) );
-		
+
 		var dataX = extractedx.matrix.elements;
-		var dataY = extractedy.matrix.elements;
-		var dataZ = extractedz.matrix.elements;		
-			
-		var entityEx = document.createElement('a-entity');
-		var entityEy = document.createElement('a-entity');
-		var entityEz = document.createElement('a-entity');
-		
-		entityEx.setAttribute('slice',{eje: 'x', dat: dataX , sp: volume.yLength, width: volume.zLength, height: volume.xLength});
-		entityEy.setAttribute('slice',{eje: 'y', dat: dataY , sp: volume.zLength, width: volume.xLength, height: volume.yLength});
-		entityEz.setAttribute('slice',{eje: 'z', dat: dataZ , sp: volume.xLength, width: volume.yLength, height: volume.zLength});
-		
-		var prism = document.querySelector('a-scene');
-		prism.appendChild(entityEx);
-		prism.appendChild(entityEy);
-		prism.appendChild(entityEz);
-	},
-	
+	 	var dataY = extractedy.matrix.elements;
+		var dataZ = extractedz.matrix.elements;
+
+        entityEx.setAttribute('slice',{eje: 'x', dat: dataX , sp: volume.yLength, width: volume.zLength, height: volume.xLength, color: '#7678ED'});
+        aframeScene.appendChild(entityEx);
+        entityEy.setAttribute('slice',{eje: 'y', dat: dataY , sp: volume.zLength, width: volume.xLength, height: volume.yLength , color: '#F18701'});
+        aframeScene.appendChild(entityEy);
+        entityEz.setAttribute('slice',{eje: 'z', dat: dataZ , sp: volume.xLength, width: volume.yLength, height: volume.zLength, color: '#A1F200'});
+        aframeScene.appendChild(entityEz);
+        console.log(entityEx , entityEy ,   entityEz , entityEl);
+
+    },
+
     init: function(){
-		var data = this.data; 
+		var data = this.data;
 		var loader = new THREE.NRRDLoader();
 		loader.load(data.volumePath, this.onLoad);
 		var el = this.el;
-		
+
     }
 
 });
@@ -57,19 +57,20 @@ AFRAME.registerComponent('slice', {
 			eje: {type: 'string'},
 			dat:{type: 'array'},
 			sp: {type: 'int'},
-			width: {type: 'array'},
-			height: {type: 'array'}
+			width: {type: 'int'},
+			height: {type: 'int'},
+			color: {type: 'string'}
 			},
-		init: function(){	
+		init: function(){
 			var el=this.el;
-			var data = this.data; 
-			
+			var data = this.data;
+
 			var geometry = new THREE.PlaneGeometry(data.width,data.height);
-			var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+			var material = new THREE.MeshBasicMaterial({color: data.color});
 			this.mesh = new THREE.Mesh(geometry, material);
 			this.planeHelper = new THREE.PlaneHelper( this.mesh );
 			el.setObject3D('mesh', this.mesh);
-			
-			console.log(this.el.sceneEl);  // Reference to the scene element.
-		}
+            console.log(this.el.sceneEl);
+
+        }
 		});
