f100dde232e9063158ebcf222bcd79b9d04ca426
Sara Arribas del Rosario <sara.arrdr@gmail.com>
2018-11-15 18:12:20 +0000

    slider

--- AFrameProject/processing.js
+++ AFrameProject/processing.js
@@ -5,13 +5,25 @@ function umbralizacion(){
     document.querySelector('#sagital').setAttribute('umbral', umbr);
     document.querySelector('#axial').setAttribute('umbral', umbr);
     document.querySelector('#volumenCompleto').setAttribute('volume', {volumePath: "data/CTChest.nrrd" , umbral : umbr}); 
-    console.log("llega aqui"); 
+    document.getElementById("range").value = umbr;
     }else{
         var umbr = document.getElementById("umbralizacion").value;
+        if(umbr == ""){
+            console.log("roto");
+        }
+        console.log(umbr);
         document.querySelector('#coronal').setAttribute('umbral', umbr);
         document.querySelector('#sagital').setAttribute('umbral', umbr);
         document.querySelector('#axial').setAttribute('umbral', umbr);
         //document.querySelector('#volumenCompleto').setAttribute('volume', {volumePath: "data/MRBrainTumor1.nrrd" , umbral : umbr});    
         document.querySelector('#volumenCompleto').setAttribute('volume', { umbral : umbr })
+        document.getElementById("range").value = umbr;
     }
+}
+
+function changeBar(){
+    var umbr = document.getElementById("umbralizacion").value;
+    var Bar = document.getElementById("range").value;
+    document.getElementById("umbralizacion").value = Bar;
+    umbralizacion();
 }
